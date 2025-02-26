7e6ab734ae22a2a7535d3d9cbe5a8e61830d70fe
Sam Goldsmith <samausmith@gmail.com>
2025-02-26 00:01:22 -0700

    removed old mime troubleshooting

--- app.js
+++ app.js
@@ -27,12 +27,6 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-app.use(express.static("public"));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
